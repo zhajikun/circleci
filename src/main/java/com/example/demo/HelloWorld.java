6d6b6682882eaa62ab2e46f0b4a1200fd8e8304f
Zha, Jikun <Jikun.Zha@charter.com>
2020-08-29 17:51:06 -0500

    initial commit

---
+++ src/main/java/com/example/demo/HelloWorld.java
@@ -0,0 +1,10 @@
+package com.example.demo;
+
+/**
+ * @author P2889307ADM
+ */
+public class HelloWorld {
+   public void sayHello(){
+     System.out.println("Hello World!!!!");
+   }
+}
---
+++ src/test/java/com/example/demo/HelloWorldTest.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class HelloWorldTest {
+
+  @Test
+  public void sayHello() {
+    assertEquals("TEst", "TEst");
+  }
+}
